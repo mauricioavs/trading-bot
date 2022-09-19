661c9d6611cffbdeda17459a3bce360b38a2319a
Mauricio Velez <mauriciovelsa99@gmail.com>
2022-09-18 21:42:52 -0500

    Improved logic of techincal indicators: Now two indicators can use same column to not make it twice. Moved Diego stuff to a folder. Working on RNN and test_strategy of main test model

--- lib/Technical Indicators.py
+++ lib/Technical Indicators.py
@@ -1,34 +1,40 @@
+
+
+
 import pandas as pd
 import numpy as np
+
 
 class SMA():
     
     def __init__(self, data, SMA_S, SMA_L, column):
         self.data = data # Dataframe
-        self.SMA_S = SMA_S # short SMA
-        self.SMA_L = SMA_L # long SMA
+        self.SMA_S = column + "|SMA|" + str(SMA_S) # short SMA
+        self.SMA_L = column + "|SMA|" + str(SMA_L) # long SMA
+        self.short = SMA_S
+        self.long = SMA_L
         self.column = column # column to use SMA
-        self.data["SMA_S"] = np.nan
-        self.data["SMA_L"] = np.nan
         
-    def calculate(self): #calculate for all dataframe
-        self.data["SMA_S"] = self.data[self.column].rolling(self.SMA_S).mean()
-        self.data["SMA_L"] = self.data[self.column].rolling(self.SMA_L).mean()
+    def calculate(self, force = False): #calculate for all dataframe
+        if not self.SMA_S in self.data.columns or force:
+            self.data[self.SMA_S] = self.data[self.column].rolling(self.short).mean()  
+        if not self.SMA_L in self.data.columns or force:    
+            self.data[self.SMA_L] = self.data[self.column].rolling(self.long).mean()
         #DONT DROP NA BECAUSE OTHER INDICATORS NEED THAT ROWS!!!
     
     def calculate_for_last_row(self): #calculate just for last row
-        self.data["SMA_S"].iloc[-self.SMA_S:] = self.data[self.column].iloc[-self.SMA_S:].rolling(self.SMA_S).mean()
-        self.data["SMA_L"].iloc[-self.SMA_L:] = self.data[self.column].iloc[-self.SMA_L:].rolling(self.SMA_L).mean()
+        self.data[self.SMA_S].iloc[-1] = self.data[self.column].iloc[-self.short:].rolling(self.short).mean()[-1]
+        self.data[self.SMA_L].iloc[-1] = self.data[self.column].iloc[-self.long:].rolling(self.long).mean()[-1]
     
     def strategy1(self, row):
         '''Returns predicted position (1,0 or -1)'''
-        if self.data["SMA_S"].iloc[row] > self.data["SMA_L"].iloc[row]: # signal to go long
+        if self.data[self.SMA_S].iloc[row] > self.data[self.SMA_L].iloc[row]: # signal to go long
             return 1
-        elif self.data["SMA_S"].iloc[row] < self.data["SMA_L"].iloc[row]: # signal to go short
+        elif self.data[self.SMA_S].iloc[row] < self.data[self.SMA_L].iloc[row]: # signal to go short
             return -1
         else:
             return 0
-        
+
 class EWMA():
     #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.ewm.html
     #approx average periods n are calculated by: n is approx 1/(1 - alpha)
@@ -41,12 +47,14 @@ class EWMA():
         self.alpha_s = 1-1/approx_avg_period_s #alpha for short EWMA
         self.alpha_l = 1-1/approx_avg_period_l #alpha for long EWMA
         self.column = column # column to use SMA
-        self.data["EWMA_S"] = np.nan
-        self.data["EWMA_L"] = np.nan
+        self.EWMA_S = column + "|EWMA|" + str(approx_avg_period_s) # short SMA
+        self.EWMA_L = column + "|EWMA|" + str(approx_avg_period_l) # long SMA
         
-    def calculate(self): #calculate for all dataframe
-        self.data["EWMA_S"] = self.data[self.column].ewm(alpha = self.alpha_s).mean()
-        self.data["EWMA_L"] = self.data[self.column].ewm(alpha = self.alpha_l).mean()
+    def calculate(self, force = False): #calculate for all dataframe
+        if not self.EWMA_S in self.data.columns or force:
+            self.data[self.EWMA_S] = self.data[self.column].ewm(alpha = self.alpha_s).mean()
+        if not self.EWMA_L in self.data.columns or force:
+            self.data[self.EWMA_L] = self.data[self.column].ewm(alpha = self.alpha_l).mean()
         #DONT DROP NA BECAUSE OTHER INDICATORS NEED THAT ROWS!!!
     def calculate_for_last_row(self): #calculate just for last row
         s = round(self.approx_avg_period_s)
@@ -56,41 +64,42 @@ class EWMA():
         p_s = max([100, s*2]) # min 100 of precision
         p_l = max([100, l*2]) # min 100 of precision
         #calculate EWMA and just update last row
-        self.data["EWMA_S"].iloc[-1:] = self.data[self.column].iloc[-p_s:].ewm(alpha = self.alpha_s).mean()[-1]
-        self.data["EWMA_L"].iloc[-1:] = self.data[self.column].iloc[-p_l:].ewm(alpha = self.alpha_l).mean()[-1]
+        self.data[self.EWMA_S].iloc[-1:] = self.data[self.column].iloc[-p_s:].ewm(alpha = self.alpha_s).mean()[-1]
+        self.data[self.EWMA_L].iloc[-1:] = self.data[self.column].iloc[-p_l:].ewm(alpha = self.alpha_l).mean()[-1]
     def strategy1(self, row):
         '''Returns predicted position (1,0 or -1)'''
-        if self.data["EWMA_S"].iloc[row] > self.data["EWMA_L"].iloc[row]: # signal to go long
+        if self.data[self.EWMA_S].iloc[row] > self.data[self.EWMA_L].iloc[row]: # signal to go long
             return 1
-        elif self.data["EWMA_S"].iloc[row] < self.data["EWMA_L"].iloc[row]: # signal to go short
+        elif self.data[self.EWMA_S].iloc[row] < self.data[self.EWMA_L].iloc[row]: # signal to go short
             return -1
         else:
             return 0
-
+        
 class BollingerBands():
     #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.ewm.html
     #approx average periods n are calculated by: n is approx 1/(1 - alpha)
     # => we are going to calculate alpha given n approx average periods as: alpha = 1- 1/n
     #Important: approx_avg_period are float in (1, inf). In (1,2) considers high weights for current day
-    def __init__(self, data, column = "price", dev = 1, SMA = 50):
+    def __init__(self, data, column = "price", dev = 1, periods = 50):
         self.data = data # Dataframe
         self.column = column #column used to calculate BBs
         self.dev = dev #standard deviations for BBs
-        self.SMA = SMA #SMA FOR BBs
+        self.SMA = column + "|BBs|" + str(dev)+"|"+str(periods) #SMA FOR BBs
         self.last_position = 0 #saves last position
-        
-    def calculate(self): #calculate for all dataframe
-        SM = self.data[self.column].rolling(self.SMA) #SMA one step before calculating mean()
-        self.data["SMA_BBs"] = SM.mean()
-        self.data["Lower"] = self.data["SMA_BBs"] - SM.std() * self.dev
-        self.data["Upper"] = self.data["SMA_BBs"] + SM.std() * self.dev
-        self.data["distance_SMA_BBs"] = self.data[self.column] - self.data.SMA_BBs 
+        self.periods = periods
+    def calculate(self, force = False): #calculate for all dataframe
+        if not self.SMA in self.data.columns or force:
+            SM = self.data[self.column].rolling(self.periods) #SMA one step before calculating mean()
+            self.data[self.SMA] = SM.mean()
+            self.data[self.SMA+"|Lower"] = self.data[self.SMA] - SM.std() * self.dev
+            self.data[self.SMA+"|Upper"] = self.data[self.SMA] + SM.std() * self.dev
+            self.data[self.SMA+"|Distance"] = self.data[self.column] - self.data[self.SMA] 
         #DONT DROP NA BECAUSE OTHER INDICATORS NEED THAT ROWS!!!
     def calculate_for_last_row(self): #calculate just for last row
         SM = self.data[self.column].iloc[-self.SMA:].rolling(self.SMA)
-        self.data["SMA_BBs"].iloc[-1:] = SM.mean()[-1]
-        self.data["Lower"].iloc[-1:] = self.data["SMA_BBs"].iloc[-1:] - SM.std()[-1] * self.dev
-        self.data["Upper"].iloc[-1:] = self.data["SMA_BBs"].iloc[-1:] + SM.std()[-1] * self.dev
+        self.data[self.SMA].iloc[-1:] = SM.mean()[-1]
+        self.data[self.SMA + "|Lower"].iloc[-1:] = self.data[self.SMA].iloc[-1:] - SM.std()[-1] * self.dev
+        self.data[self.SMA + "|Upper"].iloc[-1:] = self.data[self.SMA].iloc[-1:] + SM.std()[-1] * self.dev
         
     def strategy1(self, row):
         '''Returns predicted position (1,0 or -1)'''
@@ -99,17 +108,16 @@ class BollingerBands():
         #self.data["position"] = np.where(self.data[self.column] > self.data.Upper, -1, self.data["position"])
         #self.data["position"] = np.where(self.data.distance * self.data.distance.shift(1) < 0, 0, self.data["position"])
         #self.data["position"] = self.data.position.ffill().fillna(0) 
-        
-        if row == 0: return 0 #for first row return neutral
-        
-        if self.data[self.column].iloc[row] < self.data.Lower.iloc[row]:
+                
+        if self.data[self.column].iloc[row] < self.data[self.SMA+"|Lower"].iloc[row]:
             self.last_position = 1
             return self.last_position
-        elif self.data[self.column].iloc[row] > self.data.Upper.iloc[row]:
+        elif self.data[self.column].iloc[row] > self.data[self.SMA+"|Upper"].iloc[row]:
             self.last_position = -1 
             return self.last_position
-        elif self.data["distance_SMA_BBs"].iloc[row] * self.data["distance_SMA_BBs"].iloc[row-1] < 0:
+        elif row != 0 and self.data[self.SMA+"|Distance"].iloc[row] * self.data[self.SMA+"|Distance"].iloc[row-1] < 0:
             self.last_position = 0
             return self.last_position
         else:
             return self.last_position
+        
